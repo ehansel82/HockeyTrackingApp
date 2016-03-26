1a3c5d9777057d41c6dd6ababf006bf870920efb
Eric Hansel <e.hansel.01@gmail.com>
2016-03-26 11:14:17 -0400

    Some initial controllers and views.

---
+++ HockeyTrackingApp/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HockeyTrackingApp.Models
+{
+    public class Player
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Number { get; set; }
+    }
+}
