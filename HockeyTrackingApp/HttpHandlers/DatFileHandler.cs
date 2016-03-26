e80d2432f1269c60191ab9ab1de6416722b99b3e
Eric Hansel <e.hansel.01@gmail.com>
2016-03-26 19:35:49 -0400

    Added an httphandler that simply responds that the site does not serve .dat files if a .dat file is requested.

---
+++ HockeyTrackingApp/HttpHandlers/DatFileHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+
+namespace HockeyTrackingApp.HttpHandlers
+{
+    /// <summary>
+    /// Custom http handler for dat files.
+    /// This has the following corresponding entry in the web.config:
+    ///    <!--<add verb="*" path="*.dat" type="DatFileHandler"/>-->
+    /// </summary>
+    public class DatFileHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("This site does not serve .DAT files!");
+        }
+    }
+}
