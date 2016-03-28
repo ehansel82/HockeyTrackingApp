a70846b344dddf0f185ed0c88589e997e4b8c811
Eric Hansel <e.hansel.01@gmail.com>
2016-03-27 21:09:12 -0400

    Created a default layout "master" page.

---
+++ HockeyTrackingApp/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace HockeyTrackingApp
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js")
+                   .Include("~/Scripts/jquery-{version}.js")
+                   .IncludeDirectory("~/Scripts/app", "*.js"));
+        }
+    }
+}
